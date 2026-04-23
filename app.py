e14afa3ed926216da152878f71fad7ae778bbcf5
Dipankar Bhowmik <dkbhowmik0@gmail.com>
2026-04-23 22:10:47 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    try:
+        db = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+        return "Connected to MariaDB 🚀"
+    except Exception as e:
+        return f"DB Connection Failed ❌: {e}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
